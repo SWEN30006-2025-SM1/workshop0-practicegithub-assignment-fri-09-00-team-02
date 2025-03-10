4cd7b2bc4a773b934d796d3eb3833467a9b21400
胡湘笛 <huxiangdi@Mac.lan>
2025-03-10 23:53:21 +1100

    Making a change

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("multiply = " + calculator.multiply(3, 5));
         System.out.println("divide = " + calculator.divide(6, 3));
 
+        //new change
+        System.out.println("Hello");
+
     }
 }
